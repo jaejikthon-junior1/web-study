88c1f0a75bd1c5ddb86c0819cac2efe7465b8734
mike2827 <mike2827@hanyang.ac.kr>
2023-02-08 20:07:21 +0900

    DELETE text, FIX app.js

--- 고무성/app.js
+++ 고무성/app.js
@@ -4,4 +4,9 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.strokeRect(50, 50, 100, 200);
+ctx.moveTo(50, 50);
+ctx.lineTo(150, 50);
+ctx.lineTo(150, 150);
+ctx.lineTo(50, 150);
+ctx.lineTo(50, 50);
+ctx.fill();
