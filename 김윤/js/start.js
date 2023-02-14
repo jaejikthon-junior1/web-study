2159f9dcff6cee3e2ea02d3118782b4ee40e7fda
kimyoon <kimyun67.gmail.com>
2023-02-14 22:52:34 +0900

    restart

---
+++ 김윤/js/start.js
@@ -0,0 +1,16 @@
+/** @type {CanvasRenderingContext2D} */
+const main = document.querySelector("#main");
+const qna = document.querySelector("#qna");
+
+function begin(){
+    main.style.WebkitAnimation ="fadeOut 1s";
+    main.style.animation = "fadeOut 1s";
+    setTimeout(() => {
+        qna.style.WebkitAnimation = "fadeIn 1s";
+        qna.style.animation = "fadeIn 1s";
+        setTimeout(() => {
+            main.style.display = "none";
+            qna.style.display = "block";
+          }, 450)
+        }, 450);
+      }
